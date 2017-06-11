89829b9825e6867308ef88012a1fda74f7893768
kostetskyy <nazar.kostetskyy@gmail.com>
2017-06-11 16:15:26 +0300

    add_changes_to_files

--- lib/topics/object_model.rb
+++ lib/topics/object_model.rb
@@ -2,4 +2,24 @@
 
 # exercise object model
 module ObjectModel
+  # class Parent
+  class Parent
+    def mine_bitcoins
+      inspect
+    end
+
+    def fork_process
+      'Parent: allocate memory'
+    end
+  end
+  # module LinuxFriendly
+  module LinuxFriendly
+    def fork_process
+      super
+    end
+  end
+  # class Laptop
+  class Laptop < Parent
+    prepend LinuxFriendly
+  end
 end
--- lib/topics/state_and_behaviour.rb
+++ lib/topics/state_and_behaviour.rb
@@ -2,4 +2,59 @@
 
 # exercise state and behaviour
 module StateAndBehaviour
+  # class Car
+  class Car
+    attr_accessor :year, :color, :model, :current_speed
+
+    def self.default_car
+      Car.new
+    end
+
+    def initialize(params)
+      unless params.is_a?(Hash)
+        raise ArgumentError.new("Expects hash of attributes")
+      end
+      @year = params[:year] || default_year
+      @color = params[:color] || default_color
+      @model = params[:model] || default_model
+      @current_speed = 0
+    end
+
+    def default_color
+      @color = 'purple'
+    end
+
+    def default_model
+      @model = 'skyline'
+    end
+
+    def default_year
+      @year = 2015
+    end
+
+    def get_year
+      @year
+    end
+
+    def get_color
+      @color
+    end
+
+    def get_model
+      @model
+    end
+
+    def get_current_speed
+      @current_speed
+    end
+
+    def speed_up(speed)
+      @current_speed = current_speed + speed
+    end
+
+    def push_break(speed)
+      return unless speed <= @current_speed
+      @current_speed -= speed
+    end
+  end
 end
